b473f85de6c874db83454127e522244618049fdb
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-02-06 16:24:43 +0100

    add:ex anda changes

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,7 @@
 /*2 - hobbies.js
 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
 2. Esporta la funzione dal file.*/
+
+function hobbies(hobbyOne, hobbyTwo, hobbyThree){
+
+}
--- names.js
+++ names.js
@@ -1,2 +1,28 @@
 /*1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 2. Esporta la funzione dal file.*/
+
+/*function fullName(firstName, lastName){
+    const risultato = firstName + lastName;
+    return risultato;
+
+}
+
+const nome = fullName(firstName, lastName);
+console.log (nome);
+console.log(risultato); 
+
+/*function fullName(){
+    console.log(firsName + lastName)
+}*/
+
+/*function fullName(firstName, lastName){
+    console.log(fullName);
+
+}*/
+
+function fullName(firstName, lastName){
+    return firstName, lastName
+}
+
+// esportiamo solo la funzione fullName
+module.exports = fullName;
